c9a6a357dd59a276512286d44b5772ae0aed2249
Rasim Rakhmanov <rasim.rakhmanov@gmail.com>
2017-10-24 12:23:49 +0400

    changes after review

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
 var map;
 
-var initialLocations = [
-    {title:'San Francisco Museum of Modern Art', lat: 37.785718, lng: -122.401051, wiki:'San_Francisco_Museum_of_Modern_Art'},
-    {title:'de Young Museum', lat: 37.771469, lng: -122.468676, wiki:'De_Young_(museum)'},
-    {title:'Fine Arts Museums of San Francisco', lat: 37.771516, lng: -122.468647, wiki:'Fine_Arts_Museums_of_San_Francisco'},
-    {title:'Asian Art Museum', lat: 37.780164, lng: -122.416199, wiki:'Asian_Art_Museum_(San_Francisco)'},
-    {title:'Legion of Honor', lat: 37.784466, lng: -122.500842, wiki:'Legion_of_Honor_(museum)'},
-    {title:'Beat Museum', lat: 37.798065, lng: -122.406226, wiki:'Beat_Museum'},
-    {title:'California Academy of Sciences', lat: 37.769865, lng: -122.466095, wiki:'California_Academy_of_Sciences'},
-    {title:'San Francisco Museum and Historical Society', lat: 37.782722, lng: -122.407235, wiki:'San_Francisco_Museum_and_Historical_Society'},
-    {title:'Contemporary Jewish Museum', lat: 37.786009, lng: -122.403701, wiki:'Contemporary_Jewish_Museum'},
-    {title:'The Walt Disney Family Museum', lat: 37.801455, lng: -122.4586561, wiki:'Walt_Disney_Family_Museum'},
-    {title:'Musée Mécanique', lat: 37.809340, lng: -122.416061, wiki:'Musée_Mécanique'}
-];
-
 var ViewModel = function () {
 
   var self = this;
@@ -54,9 +40,9 @@ var ViewModel = function () {
         self.filteredLocations.push(self.locationsList()[i]);
         // console.log('filtered');
         // console.log(self.filteredLocations());
-        self.locationsList()[i].marker().setMap(map);
+        self.locationsList()[i].marker().setVisible(true);
       } else {
-        self.locationsList()[i].marker().setMap(null);
+        self.locationsList()[i].marker().setVisible(false);
       }
     }
   });
